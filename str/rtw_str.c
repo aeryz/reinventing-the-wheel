130676e593dd37647bad32a9a66e2c27125bb675
aeryz <abdullaheryz@protonmail.com>
2021-02-23 19:16:24 +0300

    feat(str): making api great

--- str/rtw_str.c
+++ str/rtw_str.c
@@ -2,41 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
-rtw_str rtw_str_new_heap(const char *str) {
-    rtw_str heap_str;
-    heap_str.type = HEAP_STR;
-    rtw_str_data data;
-    size_t len = strlen(str);
-    data.heap_str.data = (char *)malloc(len + 1);
-    data.heap_str.len = len;
-    data.heap_str.capacity = len;
-    strncpy(data.heap_str.data, str, len);
-    heap_str.data = data;
-    return heap_str;
-}
-
-void rtw_str_concat_heap(rtw_str *self, const rtw_str *other) {
-    rtw_heap_str *self_str = &self->data.heap_str;
-    const rtw_heap_str *other_str = &other->data.heap_str;
-    if (self_str->len + other_str->len > self_str->capacity) {
-        self_str->data =
-            (char *)realloc(self_str->data, self_str->len + other_str->len + 1);
-    }
-    strncpy(self_str->data + self_str->len, other_str->data, other_str->len);
-    self_str->len += other_str->len;
-    self_str->capacity += other_str->len;
-}
-
 rtw_str rtw_str_new() {
     rtw_str str;
-    rtw_str_data data;
+    rtw_str_data_ data;
     memset(data.stack_str, 0, sizeof(data.stack_str));
     str.type = STACK_STR;
     str.data = data;
     return str;
 }
 
-int rtw_str_from(char *data, rtw_str *out) {
+int rtw_str_from(const char *data, rtw_str *out) {
     size_t len = strlen(data);
     if (len > sizeof(rtw_heap_str) - 1) {
         out->type = HEAP_STR;
@@ -57,23 +32,8 @@ int rtw_str_from(char *data, rtw_str *out) {
     return 0;
 }
 
-char *rtw_str_get_data(rtw_str *self) {
-    if (self->type == HEAP_STR)
-        return self->data.heap_str.data;
-    else
-        return self->data.stack_str;
-}
-
-size_t rtw_str_len(rtw_str *self) {
-    if (self->type == HEAP_STR)
-        return self->data.heap_str.len;
-    else
-        return sizeof(self->data) -
-               self->data.stack_str[sizeof(self->data) - 1] - 1;
-}
-
-int rtw_str_concat(rtw_str *self, rtw_str *other) {
-    char *other_str = rtw_str_get_data(other);
+int rtw_str_concat(rtw_str *self, const rtw_str *other) {
+    const char *other_str = rtw_str_data(other);
     size_t other_len = rtw_str_len(other);
     size_t self_len = rtw_str_len(self);
     if (self->type == HEAP_STR) {
@@ -114,9 +74,133 @@ int rtw_str_concat(rtw_str *self, rtw_str *other) {
     return 0;
 }
 
-void rtw_str_debug(rtw_str *self) {
+size_t rtw_str_len(const rtw_str *self) {
     if (self->type == HEAP_STR)
-        printf("%s\n", self->data.heap_str.data);
+        return self->data.heap_str.len;
     else
-        printf("%s\n", self->data.stack_str);
+        return sizeof(self->data) -
+               self->data.stack_str[sizeof(self->data) - 1] - 1;
+}
+
+size_t rtw_str_capacity(const rtw_str *self) {
+    if (self->type == HEAP_STR)
+        return self->data.heap_str.capacity;
+    else
+        return sizeof(self->data.heap_str);
+}
+
+// TODO: unimpl
+int rtw_str_reserve(rtw_str *self, size_t n) {
+    if (rtw_str_capacity(self) > n) {
+        return 0;
+    }
+    return 1;
+}
+
+void rtw_str_clear(rtw_str *self) {
+    if (rtw_str_empty(self))
+        return;
+    if (self->type == HEAP_STR) {
+        self->data.heap_str.data[0] = '\0';
+        self->data.heap_str.len = 0;
+    } else {
+        self->data.stack_str[0] = '\0';
+        self->data.stack_str[sizeof(rtw_heap_str) - 1] =
+            sizeof(rtw_heap_str) - 1;
+    }
+}
+
+int rtw_str_empty(const rtw_str *self) {
+    if (self->type == HEAP_STR)
+        return self->data.heap_str.len == 0;
+    else
+        return self->data.stack_str[sizeof(rtw_heap_str) - 1] ==
+               sizeof(rtw_heap_str) - 1;
+}
+
+int rtw_str_shrink_to_fit(rtw_str *self) {
+    if (self->type == STACK_STR ||
+        self->data.heap_str.len == self->data.heap_str.capacity) {
+        return 0;
+    }
+    char *new_ptr =
+        (char *)realloc(self->data.heap_str.data, self->data.heap_str.len);
+    if (!new_ptr)
+        return -1;
+    self->data.heap_str.data = new_ptr;
+    self->data.heap_str.capacity = self->data.heap_str.len;
+    return 0;
+}
+
+char rtw_str_at(rtw_str *self, size_t n) {
+    if (rtw_str_len(self) < n) {
+        return 0;
+    } else {
+        return rtw_str_data(self)[n];
+    }
+}
+
+// TODO: unimpl
+int rtw_str_push_back(rtw_str *self, char c) { return 0; }
+
+// TODO: unimpl
+int rtw_str_copy(rtw_str *self, const rtw_str *other);
+
+char rtw_str_pop_back(rtw_str *self) {
+    if (rtw_str_empty(self)) {
+        return '\0';
+    }
+    char ret;
+    if (self->type == HEAP_STR) {
+        ret = self->data.heap_str.data[self->data.heap_str.len - 1];
+        self->data.heap_str.data[self->data.heap_str.len-- - 1] = '\0';
+    } else {
+        ret =
+            self->data
+                .stack_str[sizeof(rtw_heap_str) -
+                           self->data.stack_str[sizeof(rtw_heap_str) - 1] - 1];
+        self->data
+            .stack_str[sizeof(rtw_heap_str) -
+                       self->data.stack_str[sizeof(rtw_heap_str) - 1]++ - 1] =
+            '\0';
+    }
+    return ret;
+}
+
+const char *rtw_str_data(const rtw_str *self) {
+    if (self->type == HEAP_STR)
+        return self->data.heap_str.data;
+    else
+        return self->data.stack_str;
+}
+
+int rtw_str_compare(const rtw_str *self, const rtw_str *other) {
+    return strcmp(rtw_str_data(self), rtw_str_data(other));
+}
+
+// For benches
+
+rtw_str rtw_str_new_heap(const char *str) {
+    rtw_str heap_str;
+    heap_str.type = HEAP_STR;
+    rtw_str_data_ data;
+    size_t len = strlen(str);
+    data.heap_str.data = (char *)malloc(len + 1);
+    data.heap_str.len = len;
+    data.heap_str.capacity = len;
+    strncpy(data.heap_str.data, str, len);
+    heap_str.data = data;
+    return heap_str;
+}
+
+void rtw_str_concat_heap(rtw_str *self, const rtw_str *other) {
+    rtw_heap_str *self_str = &self->data.heap_str;
+    const rtw_heap_str *other_str = &other->data.heap_str;
+    if (self_str->len + other_str->len > self_str->capacity) {
+        self_str->data =
+            (char *)realloc(self_str->data, self_str->len + other_str->len + 1);
+    }
+    strncpy(self_str->data + self_str->len, other_str->data, other_str->len);
+    self_str->len += other_str->len;
+    self_str->capacity += other_str->len;
 }
--- str/rtw_str.h
+++ str/rtw_str.h
@@ -12,27 +12,69 @@ typedef struct {
 typedef union {
     rtw_heap_str heap_str;
     char stack_str[sizeof(rtw_heap_str)];
-} rtw_str_data;
+} rtw_str_data_;
 
 typedef enum { HEAP_STR, STACK_STR } rtw_str_type;
 
 typedef struct {
     rtw_str_type type;
-    rtw_str_data data;
+    rtw_str_data_ data;
 } rtw_str;
+
+/** Creates an empty string without any allocation. */
+rtw_str rtw_str_new();
+
+/** Creates a string from the given char*. */
+int rtw_str_from(const char *data, rtw_str *out);
+
+/** Adds 'other' to 'self'. */
+int rtw_str_concat(rtw_str *self, const rtw_str *other);
+
+/** Returns the length of 'self'. */
+size_t rtw_str_len(const rtw_str *self);
+
+/** Returns the capacity of 'self'. For stack allocated strings,
+ * it returns the maximum stack allocation length. */
+size_t rtw_str_capacity(const rtw_str *self);
+
+/** Expands the capacity of 'self'. If 'n' is less than MAX_STACK_LEN,
+ * or current capacity, it does nothing. */
+int rtw_str_reserve(rtw_str *self, size_t n);
+
+/** Erases 'self' by writing null terminator to the first character and
+ * changing the length to zero. */
+void rtw_str_clear(rtw_str *self);
+
+/** Returns if 'self' is empty or not. */
+int rtw_str_empty(const rtw_str *self);
+
+/** Reduces the capacity of 'self' to fit its 'size'. */
+int rtw_str_shrink_to_fit(rtw_str *self);
+
+/** Returns the 'n'th character of 'self'. Returns '\0' if 'n' exceeds the size
+ * of 'self'. */
+char rtw_str_at(rtw_str *self, size_t n);
+
+/** Appends 'c' to 'self'. */
+int rtw_str_push_back(rtw_str *self, char c);
+
+/** Copies the contents of 'other' to 'self'. */
+int rtw_str_copy(rtw_str *self, const rtw_str *other);
+
+/** Pops and returns the last character of 'self'. If 'self' is empty, returns
+ * '\0'. */
+char rtw_str_pop_back(rtw_str *self);
+
+/** Returns the underlying char* of 'self'. */
+const char *rtw_str_data(const rtw_str *self);
+
+/** Wrapper of 'strcmp'. */
+int rtw_str_compare(const rtw_str *self, const rtw_str *other);
+
+// For benches
 
 rtw_str rtw_str_new_heap(const char *str);
 
 void rtw_str_concat_heap(rtw_str *self, const rtw_str *other);
-
-rtw_str rtw_str_new();
-
-int rtw_str_from(char *data, rtw_str *out);
-
-int rtw_str_concat(rtw_str *self, rtw_str *other);
-
-size_t rtw_str_len(rtw_str *self);
-
-void rtw_str_debug(rtw_str *self);
 
 #endif
