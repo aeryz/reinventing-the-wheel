1a27c2549bd565380f0e3f29b13cec7892ffe173
aeryz <abdullaheryz@protonmail.com>
2021-02-23 14:36:12 +0300

    refactor directory structure

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
-#include "ctl_str.h"
+#include "str/rtw_str.h"
 #include <stdio.h>
 
-
 int main() {
-	ctl_str str1;
-	ctl_str str2;
-	ctl_str_from("selameke", &str1);
-	ctl_str_from("cool and very very very long strrrssss", &str2);
-	ctl_str_debug(&str1);
-	ctl_str_debug(&str2);
-	ctl_str_concat(&str1, &str2);
-	ctl_str_debug(&str1);
-	ctl_str_concat(&str2, &str1);
-	ctl_str_debug(&str2);
+    rtw_str str1;
+    rtw_str str2;
+    rtw_str_from("selameke", &str1);
+    rtw_str_from("cool and very very very long strrrssss", &str2);
+    rtw_str_debug(&str1);
+    rtw_str_debug(&str2);
+    rtw_str_concat(&str1, &str2);
+    rtw_str_debug(&str1);
+    rtw_str_concat(&str2, &str1);
+    rtw_str_debug(&str2);
 }
